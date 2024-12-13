f4fba006f4ca21378aa589fecbc4d28bb9e8eb7d
IINaGiBaToRII <71980719+IINaGiBaToRII@users.noreply.github.com>
2024-12-13 11:19:06 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     long_description=open('README.md', 'r').read(),
     long_description_content_type='text/markdown',
     install_requires=[
-        'selenium~=4.8.0',
+        'selenium',
         'pydub~=0.25.1',
         'SpeechRecognition~=3.8.1',
         'requests>=2.28.1,<2.32.0',
